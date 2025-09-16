5e56f32e209461482b8aa1b4e3d683bc824c7814
Martin Hallberg <88983960+MartinJHallberg@users.noreply.github.com>
2025-09-16 11:32:38 +0200

    Added pdf and test for incorrect deposit

--- tests/test_contract_loading.py
+++ tests/test_contract_loading.py
@@ -19,3 +19,4 @@ def test_load_and_extract_contract_info():
     file_path = "src/data/contract_template_with_info_printed.pdf"
     extracted_contract_info = load_contract_and_extract_info(file_path)
     assert isinstance(extracted_contract_info, ContractInfo)
+
